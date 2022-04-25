42a59a85064c9d95716a322c13d80dd29fe187bb
Oleg Vorobchikov <o.vorobchikov@rt-solar.ru>
2022-04-25 16:36:25 +0300

    Решение задачи VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,45 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const VueCalculator = defineComponent({
+  name: 'VueCalculator',
+
+  data() {
+    return {
+      operands: {
+        a: 0,
+        b: 0,
+      },
+      operator: 'sum',
+    };
+  },
+
+  computed: {
+    result() {
+      try {
+        switch (this.operator) {
+          case 'sum':
+            return this.operands.a + this.operands.b;
+            break;
+          case 'subtract':
+            return this.operands.a - this.operands.b;
+            break;
+          case 'multiply':
+            return this.operands.a * this.operands.b;
+            break;
+          case 'divide':
+            return this.operands.a / this.operands.b;
+            break;
+
+          default:
+            throw 'Unknown operator';
+        }
+      } catch (e) {
+        return e.message;
+      }
+    },
+  },
+});
+
+const app = createApp(VueCalculator);
+
+app.mount('#app');
