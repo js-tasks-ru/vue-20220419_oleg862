68b49dbc7846d3a65b2f24a2a35d38274c559e6c
Oleg Vorobchikov <o.vorobchikov@rt-solar.ru>
2022-05-04 21:04:27 +0300

    Решение задачи MeetupInfo

--- 02-components/04-MeetupInfo/MeetupInfo.js
+++ 02-components/04-MeetupInfo/MeetupInfo.js
@@ -3,19 +3,48 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 export default defineComponent({
   name: 'MeetupInfo',
 
+  props: {
+    organizer: {
+      type: String,
+      require: true,
+    },
+    place: {
+      type: String,
+      require: true,
+    },
+    date: {
+      type: Number,
+      require: true,
+    },
+  },
+
+  methods: {
+    dateAttr() {
+      return new Date(this.date).toJSON().split('T')[0];
+    },
+
+    dateText() {
+      return new Date(this.date).toLocaleDateString(navigator.language, {
+        day: 'numeric',
+        year: 'numeric',
+        month: 'short',
+      });
+    },
+  },
+
   template: `
     <ul class="meetup-info">
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{ organizer }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{ place }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time :datetime="dateAttr()">{{ dateText() }}</time>
       </li>
     </ul>`,
 });
