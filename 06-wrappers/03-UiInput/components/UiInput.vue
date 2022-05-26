e08a6c2fe4ba720adab977f515cd474a3c5e36e8
Oleg Vorobchikov <o.vorobchikov@rt-solar.ru>
2022-05-27 01:08:47 +0300

    Решение задачи UiInput

--- 06-wrappers/03-UiInput/components/UiInput.vue
+++ 06-wrappers/03-UiInput/components/UiInput.vue
@@ -1,13 +1,43 @@
 <template>
-  <div class="input-group input-group_icon input-group_icon-left input-group_icon-right">
-    <div class="input-group__icon">
-      <img class="icon" alt="icon" />
+  <div
+    class="input-group"
+    :class="{
+      'input-group_icon': $slots['left-icon'] || $slots['right-icon'],
+      'input-group_icon-left': $slots['left-icon'],
+      'input-group_icon-right': $slots['right-icon'],
+    }"
+  >
+    <div v-if="$slots['left-icon']" class="input-group__icon">
+      <slot name="left-icon" />
     </div>
 
-    <input ref="input" class="form-control form-control_rounded form-control_sm" />
+    <input
+      v-if="!multiline"
+      ref="input"
+      v-bind="$attrs"
+      v-model="customValue"
+      class="form-control"
+      :class="{
+        'form-control_rounded': rounded,
+        'form-control_sm': small,
+      }"
+    />
 
-    <div class="input-group__icon">
-      <img class="icon" alt="icon" />
+    <textarea
+      v-if="multiline"
+      ref="input"
+      v-bind="$attrs"
+      v-model="customValue"
+      class="form-control"
+      :class="{
+        'form-control_rounded': rounded,
+        'form-control_sm': small,
+      }"
+    >
+    </textarea>
+
+    <div v-if="$slots['right-icon']" class="input-group__icon">
+      <slot name="right-icon" />
     </div>
   </div>
 </template>
@@ -15,6 +45,35 @@
 <script>
 export default {
   name: 'UiInput',
+
+  inheritAttrs: false,
+
+  props: {
+    modelValue: String,
+    small: Boolean,
+    rounded: Boolean,
+    multiline: Boolean,
+  },
+
+  emits: ['update:modelValue'],
+
+  computed: {
+    customValue: {
+      get() {
+        return this.modelValue;
+      },
+
+      set(value) {
+        this.$emit('update:modelValue', value);
+      },
+    },
+  },
+
+  methods: {
+    focus() {
+      this.$refs.input.focus();
+    },
+  },
 };
 </script>
 
