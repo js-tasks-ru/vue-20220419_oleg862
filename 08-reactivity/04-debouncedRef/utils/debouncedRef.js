a38f8e0ef7daef9f7cc1b98a1cf524529b489d5d
Oleg Vorobchikov <o.vorobchikov@rt-solar.ru>
2022-06-01 01:06:22 +0300

    Задачи reactivity

--- 08-reactivity/04-debouncedRef/utils/debouncedRef.js
+++ 08-reactivity/04-debouncedRef/utils/debouncedRef.js
@@ -8,7 +8,13 @@ import debounce from 'lodash/debounce';
  * @returns {Ref<T>} - Новый ref, обновляющийся с debounce при обновлении исходного ref-а
  */
 export function debouncedRef(source, wait) {
-  const debounced = ref(undefined); // ...
-  // ...
+  const debounced = ref(undefined);
+  const fn = debounce(() => {
+    debounced.value = source.value;
+  }, wait);
+
+  watch(source, fn);
+
+  debounced.value = source.value;
   return debounced;
 }
