c893ffa1e3d54a77116071e27e836fc6623759e4
Oleg Vorobchikov <o.vorobchikov@rt-solar.ru>
2022-06-07 18:46:45 +0300

    Решение задачи PromiseWrapper

--- 10-slots/02-PromiseWrapper/components/PromiseWrapper.vue
+++ 10-slots/02-PromiseWrapper/components/PromiseWrapper.vue
@@ -1,5 +1,5 @@
 <template>
-  <!-- -->
+  <slot :name="status" :result="result" :error="error" />
 </template>
 
 <script>
@@ -10,6 +10,32 @@ export default {
     promise: {
       type: Promise,
       required: true,
+    },
+  },
+
+  data() {
+    return {
+      status: null,
+      result: null,
+      error: null,
+    };
+  },
+
+  watch: {
+    promise: {
+      immediate: true,
+      handler(promise) {
+        this.status = 'pending';
+        this.promise
+          .then((meetup) => {
+            this.result = meetup;
+            this.status = 'fulfilled';
+          })
+          .catch((error) => {
+            this.error = error;
+            this.status = 'rejected';
+          });
+      },
     },
   },
 };
